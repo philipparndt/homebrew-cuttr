97081bf7de6e584bcc04bdf95b237df10b1164f5
Philipp Arndt <2f.mail@gmx.de>
2026-08-19 16:02:29 +0200

    cuttr 0.1.0

---
+++ Casks/cuttr.rb
@@ -0,0 +1,24 @@
+cask "cuttr" do
+  version "0.1.0"
+  sha256 "0dcf59d6b99366d6cf3276c9fb182d4ac118b523533f140a603e44abe96a6e52"
+
+  url "https://github.com/philipparndt/cuttr/releases/download/v#{version}/cuttr-#{version}.dmg"
+  name "cuttr"
+  desc "Cutting room for video that keeps its decisions in plain text"
+  homepage "https://github.com/philipparndt/cuttr"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "cuttr.app"
+
+  zap trash: [
+    "~/Library/Application Support/de.rnd7.cuttr",
+    "~/Library/Preferences/de.rnd7.cuttr.plist",
+    "~/Library/Saved Application State/de.rnd7.cuttr.savedState",
+  ]
+end
