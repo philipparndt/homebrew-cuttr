317ab564c8439cb91b4d3a649509de278ae04e0d
Philipp Arndt <2f.mail@gmx.de>
2026-08-22 11:15:09 +0200

    cuttr 0.8.0

--- Casks/cuttr.rb
+++ Casks/cuttr.rb
@@ -1,6 +1,6 @@
 cask "cuttr" do
-  version "0.7.2"
-  sha256 "8b483ee01e076a21e9ade5539a3705aa21534e6cac6fbcd02be3009192dbff38"
+  version "0.8.0"
+  sha256 "78423b91ac1ae08f61d9c97505b11307ce38e7cf4d5d1d6942c624baa95d46af"
 
   url "https://github.com/philipparndt/cuttr/releases/download/v#{version}/cuttr-#{version}.dmg"
   name "cuttr"
